f66d5488d2c2d51216e73c50ecdc54108e719df8
William Suarez <147040637+Suarwill@users.noreply.github.com>
2023-10-11 20:23:52 -0300

    Update BD_Pedidos.sql

--- BD_Pedidos.sql
+++ BD_Pedidos.sql
@@ -1,82 +1,66 @@
--- Con comandos MySQL
-
-create table Fabrica(
-	Id_fabrica 	number primary key,
-	Direccion 	varchar2(20),
-	Comuna	varchar2(20),
-	Telefono	number,
-	Nombre		varchar2(20)				);
-create table Producto(
-	Id_prod		number primary key,
-	Def_prod	varchar2(30),
-	Precio		number					);
-create table Cliente (
-	Id_cliente 	number primary key,
-	Nom_Cliente 	varchar2(20),
-	Direccion	varchar2(20),
-	Comuna	varchar2(15),
-	Ciudad 		varchar2(15),
-	Tel_Cliente 	number,
-	Saldo		number,
-	Lim_credito	number,
-	Descuento	varchar2(20)				);
-create table Pedido(
-	Id_pedido	number,
-	Fecha 		date,
-	Id_Cliente	number,
-	Id_prod 		number,
-	Cantidad	number,
-	constraint pkidped 	primary key (Id_pedido),
-	constraint fkcli 		foreign key (Id_cliente) references Cliente(Id_cliente),
-	constraint fkidpro 	foreign key (Id_prod) references Producto(Id_prod)	);
-create table GuiaCompra(
-	ID_guia 	number,
-	Fecha 		date,
-	Id_pedido	number,
-	Id_fabrica 	number,
-	constraint pkgcomp 	primary key (ID_guia),
-	constraint fkped 	foreign key (Id_pedido) references Pedido(Id_pedido),
-	constraint fkfab 	foreign key (Id_fabrica ) references Fabrica(Id_fabrica )	);
-desc Fabrica;
-desc Producto;
-desc Cliente;
-desc Pedido;
-desc GuiaCompra;
+-- BD realizada con comandos MySQL
+CREATE TABLE ciudad
+	(codigo number(6) primary key, nomciudad varchar2(30));
+CREATE TABLE conductor
+	(rut varchar2(10) primary key,nombre varchar2(50), fono varchar2(15), direc varchar2(50),comuna varchar2(50), salario number(8,2));
+CREATE TABLE camion
+	(patente varchar2(6) primary key, modelo varchar2(20),tipo varchar2(20),potencia number(4));
+CREATE TABLE paquete
+	(numero number(6), descrip varchar2(50),destin varchar2(50),dirdestin varchar2(50), rut_cond varchar2(10),codigo_dest number(6),
+	CONSTRAINT pknum primary key(numero),
+        CONSTRAINT fkrut FOREIGN KEY(rut_cond) REFERENCES conductor(rut),
+        CONSTRAINT fkcod FOREIGN KEY(codigo_dest) REFERENCES ciudad(codigo));
+CREATE TABLE ruta
+	(fecha varchar2(10),hora varchar2(5),patente varchar2(6),rut varchar2(10),
+        CONSTRAINT fkrt FOREIGN KEY(rut) REFERENCES conductor(rut),
+        CONSTRAINT fkpat FOREIGN KEY(patente) REFERENCES camion(patente));
+DESC paquete;
+DESC ciudad;
+DESC conductor;
+DESC camion;
+DESC ruta;
 
 -- Datos no reales ingresados
-INSERT INTO Fabrica VALUES (10500,'AGAMOS 1320','BELLOTO',322517878,'COSEBET');
-INSERT INTO Fabrica VALUES (10501,'TULIPAN 103','CONSTITUCION',541532899,'COSECON');
+INSERT INTO Ciudad VALUES (251300,'SUCURSAL BELLOTO');
+INSERT INTO Ciudad VALUES (272400,'SUCURSAL CASABLANCA');
+INSERT INTO Ciudad VALUES (275100,'SUCURSAL TALCA');
+INSERT INTO Ciudad VALUES (280800,'SUCURSAL TEMUCO');
+INSERT INTO Ciudad VALUES (185200,'SUCURSAL CABILDO');
 
-INSERT INTO Producto VALUES(10951,'CORTADORA DE PAN LICUATOR',10500);
-INSERT INTO Producto VALUES(10952,'DEPORTIVO TERRENEITOR',26990);
-INSERT INTO Producto VALUES(10953,'LICUADORA MULTICULTURAL PLOP',35950);
-INSERT INTO Producto VALUES(10954,'SILLA 3 PATAS TABULE',9300);
-INSERT INTO Producto VALUES(10955,'MESA 2 PATAS BLANQUEZ',16500);
+INSERT INTO Conductor VALUES(18752463,'Manuel Perez','945458723','Angamos 1520','Villa Alemana',980200);
+INSERT INTO Conductor VALUES(14256895,'Diego Gonzalez','945256895','Petunias 620','Casablanca',1050950);
+INSERT INTO Conductor VALUES(13568993,'Mario Hugo Tais','978458569','Calbuco 312','Talca',1152300);
+INSERT INTO Conductor VALUES(15245869,'Humberto Vargas','952635698','Terramar 252','Portales',1462500);
+INSERT INTO Conductor VALUES(21356984,'Dyan Leyton','945287596','Tucson 754','Cabildo',952000);
 
-INSERT INTO Cliente VALUES (15973452,'ANDRES','BARRANCA ABAJO 68','CASABLANCA','VALPARAISO',964647878,950000,1000000,'10%');
-INSERT INTO Cliente VALUES (19584236,'MARIA','SOLAPA 125','QUINTERO','VALPARAISO',978545689,350000,1500000,'15%');
-INSERT INTO Cliente VALUES (20596569,'VIANCA','COLTEPAL 1059','LINARES','MAULE',984849696,500000,1500000,'15%');
+INSERT INTO Camion VALUES (TSHX45,'BMW','CAMION',740);
+INSERT INTO Camion VALUES (SHYX78,'VOLVO','CAMION',840);
+INSERT INTO Camion VALUES (PQTR55,'SHAM','CAMION',820);
+INSERT INTO Camion VALUES (TTFF84,'VOLVO','CAMION',720);
+INSERT INTO Camion VALUES (PRSS56,'BMW','CAMION',740);
 
-INSERT INTO Pedido VALUES (510,'10-JUL-2023',15973452,10952,8);
-INSERT INTO Pedido VALUES (511,'10-JUL-2023',20596569,10954,3);
+INSERT INTO Paquete VALUES (154890,'Encomienda 150 KG','Padre Las Casas','Tuca Tuca 45236',13568993,280800);
+INSERT INTO Paquete VALUES (256300,'Encomienda 35 Litros','Quilpue','Imperiales 4523',14256895,251300);
+INSERT INTO Paquete VALUES (254800,'Pallet 3 m3','La Ligua','Av a la Costa 7850',21356984,185200);
 
-INSERT INTO GuiaCompra VALUES (4900,'11-JUL-2023',510,10500);
-INSERT INTO GuiaCompra VALUES (4910,'11-JUL-2023',511,10501);
+INSERT INTO Ruta VALUES ('11-JUL-2023','09:20',PQTR55,13568993);
+INSERT INTO Ruta VALUES ('10-JUL-2023','11:35',PRSS56,14256895);
+INSERT INTO Ruta VALUES ('13-JUL-2023','07:20',TSHX45,21356984);
 
-SELECT * FROM Fabrica;
-SELECT * FROM Producto;
-SELECT * FROM Cliente;
-SELECT * FROM Pedido;
-SELECT * FROM GuiaCompra;
+SELECT * FROM Ciudad;
+SELECT * FROM Conductor;
+SELECT * FROM Camion;
+SELECT * FROM Paquete;
+SELECT * FROM Ruta;
 
--- Obtener el nombre de los clientes de la ciudad de Linares
-SELECT Nom_Cliente FROM Cliente 
-	WHERE Comuna = 'LINARES';
-
--- Obtener el nombre de todos los artículos solicitados por Andres
-SELECT P.Def_prod FROM Producto P, Pedido B, Cliente C
-	WHERE P.Id_prod = B.Id_prod AND B.Id_cliente = C.Id_cliente	AND C.Nom_Cliente = 'ANDRES';
-
---Obtener el ID cliente que han pedido una cantidad mayor a 5 unidades de productos
-SELECT B.Id_cliente FROM Pedido B
-	WHERE B.Cantidad > 5;
+-- Obtener el nombre de los conductores de la ciudad de Talca
+SELECT nombre FROM Conductor 
+	WHERE Comuna = 'Talca';
+-- Obtener el destino de paquete transportado por Dyan
+SELECT p.destin FROM paquete p
+	INNER JOIN conductor c ON p.rut_cond = c.rut
+	WHERE c.nombre = "Dyan Leyton";	
+--Obtener el codigo del paquete transportado en la ruta del 13 de Julio
+SELECT r.patente, r.rut, p.numero FROM ruta r
+	INNER JOIN paquete p ON r.rut = p.rut_cond
+	WHERE r.fecha = '13-JUL-2023';
